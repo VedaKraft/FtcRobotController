951ae1ab6891e67d8b77d259be2eb748aba1f6e8
VedaKraft <158237599+VedaKraft@users.noreply.github.com>
2026-06-02 18:54:41 -0500

    adding centered tag code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/AprilTagLimelightTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/AprilTagLimelightTest.java
@@ -7,6 +7,7 @@ import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
